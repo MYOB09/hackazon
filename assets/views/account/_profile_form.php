0577fc4f1d907ac7da6c48111ad7486ef1652cfd
Argyris <vassilopargiris@gmail.com>
2024-12-15 17:32:26 +0200

    Update _profile_form.php
    tabindex removed

--- assets/views/account/_profile_form.php
+++ assets/views/account/_profile_form.php
@@ -1,48 +1,44 @@
-<?php $baseImgPath = $this->pixie->getParameter('parameters.use_external_dir') ? '/upload/download.php?image=' : '/user_pictures/'; ?>
+<?php 
+$baseImgPath = $this->pixie->getParameter('parameters.use_external_dir') ? '/upload/download.php?image=' : '/user_pictures/';
+?>
 <form role="form" method="post" class="profile-edit-form" action="/account/profile/edit" id="editProfileForm" enctype="multipart/form-data">
     <?php $_token('profile'); ?>
     <div class="row">
         <div class="col-xs-6 col-sm-6 col-md-6">
             <div class="form-group">
-                <input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="First Name" tabindex="1" value="<?php $_($first_name); ?>">
+                <input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="First Name" value="<?php $_($first_name); ?>">
             </div>
         </div>
         <div class="col-xs-6 col-sm-6 col-md-6">
             <div class="form-group">
-                <input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="Last Name" tabindex="2" value="<?php $_($last_name); ?>">
+                <input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="Last Name" value="<?php $_($last_name); ?>">
             </div>
         </div>
     </div>
-
     <div class="form-group">
-        <input type="text" name="user_phone" id="user_phone" class="form-control input-lg" placeholder="Phone" tabindex="3" value="<?php $_($user_phone); ?>">
+        <input type="text" name="user_phone" id="user_phone" class="form-control input-lg" placeholder="Phone" value="<?php $_($user_phone); ?>">
     </div>
-
     <?php if (isset($photoUrl) && $photoUrl): ?>
         <div class="form-group">
             <img src="<?php echo $baseImgPath; $_($photoUrl); ?>" alt="" class="profile-picture" /> <br>
             <label><input type="checkbox" name="remove_photo" /> Remove photo</label>
         </div>
     <?php endif; ?>
-
     <div class="form-group">
-        <input type="file" name="photo" id="photo" class="file-input btn btn-default btn-primary btn-lg" title="Select avatar image" tabindex="4" value="<?php $_($photo); ?>">
+        <input type="file" name="photo" id="photo" class="file-input btn btn-default btn-primary btn-lg" title="Select avatar image" value="<?php $_($photo); ?>">
     </div>
-
     <hr class="colorgraph">
     <div class="row">
         <div class="col-xs-6 col-md-6">
-            <input type="submit" name="_submit_save" value="Save" class="btn btn-block btn-lg" tabindex="7">
+            <input type="submit" name="_submit_save" value="Save" class="btn btn-block btn-lg">
         </div>
         <div class="col-xs-6 col-md-6">
-            <input type="submit" name="_submit_save_and_exit" value="Save and Exit" class="btn btn-primary btn-block btn-lg" tabindex="8">
+            <input type="submit" name="_submit_save_and_exit" value="Save and Exit" class="btn btn-primary btn-block btn-lg">
         </div>
     </div>
 </form>
-
 <script>
     $(function() {
-
         jQuery(function($) {
             $('#editProfileForm').bootstrapValidator({
                 feedbackIcons: {
